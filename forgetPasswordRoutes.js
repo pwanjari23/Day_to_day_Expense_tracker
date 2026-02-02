25ff5861d8e7244954ea13f2a8702d6c82c35273
Pratiksha Wanjari <127431343+pwanjari23@users.noreply.github.com>
2026-02-02 16:07:33 +0530

    Sending Reset Password Link

--- forgetPasswordRoutes.js
+++ forgetPasswordRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const forgetPasswordController = require("../controllers/forgetPasswordController");
 
 router.post("/forgotpassword", forgetPasswordController.forgetPassword);
+router.post("/resetpassword/:id", forgetPasswordController.resetPassword);
+
 
 module.exports = router;
